7405616d4e9c21532d1c2acafce6226734c31fac
Jake Atakhanov <J.atahanov@yahoo.com>
2019-02-03 22:51:23 -0500

    Add files via upload

---
+++ Clock/main.js
@@ -0,0 +1,20 @@
+// const secondHand = document.querySelector('.second');
+// const minsHand = document.querySelector('.minute');
+// const hourHand = document.querySelector('.hour')
+
+// function setDate(){
+// const now = new Date();
+// const seconds = now.getSeconds();
+// const secondsDegrees = ((seconds/60) * 360) + 90;
+// secondHand.styles.transform=`rotate(${secondsDegrees}deg)`;
+
+// const mins = now.getMinutes();
+// const minsDegrees = ((mins/60)*360) + 90;
+// minsHand.styles.transform=`rotate(${minsDegrees}deg)`;
+
+// const hours = now.getHours();
+// const hoursDegrees = ((hours/12)*360) + 90;
+// hourHand.styles.transform=`rotate(${hourDegrees}deg)`
+// }
+
+// setInterval(setDate, 1000);
